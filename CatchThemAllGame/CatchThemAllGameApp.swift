4dd07e32bb59df114545fb4a4801e6518e5a0f4b
Tanya Alvarez-Salsedo <tanyaalvsal@tamu.edu>
2024-03-17 02:31:14 -0500

    Initial Commit

---
+++ CatchThemAllGame/CatchThemAllGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchThemAllGameApp.swift
+//  CatchThemAllGame
+//
+//  Created by Alvarez-Salsedo, Tanya on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CatchThemAllGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
